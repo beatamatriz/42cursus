d4f793253b9d166a97d38684d89f66e1c95ab2ea
Beatriz Batista Ballesteros <bbatista@c1r4s6.42malaga.com>
2023-12-07 17:21:36 +0100

    strlcat perfe

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,30 +6,35 @@
 /*   By: bbatista <bbatista@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 12:00:53 by bbatista          #+#    #+#             */
-/*   Updated: 2023/11/28 18:59:27 by bbatista         ###   ########.fr       */
+/*   Updated: 2023/12/07 16:33:23 by bbatista         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	catsize;
+	size_t	dstlen;
 
-	catsize = ft_strlen(dst);
+	dstlen = ft_strlen(dst);
+	if (dstsize == 0 || dstsize < dstlen)
+		catsize = dstsize;
+	else
+		catsize = ft_strlen(dst);
 	catsize += ft_strlcpy(dst + catsize, src, dstsize - catsize);
 	return (catsize);
 }
-
+/*
 int	main(void)
 {
 	char	src[5] = "abcd";
-	char	dst1[16] = "pqrstuvwxyz";
-	char	dst2[16] = "pqrstuvwxyz";
+	char	dst1[20] = "pqrstuvwxyz";
+	char	dst2[20] = "pqrstuvwxyz";
 	size_t	n1;
 	size_t	n2;
 
-	n1 = strlcat(dst1, src, 0);
-	n2 = ft_strlcat(dst2, src, 0);
-	printf("%zu: %s, %zu: %s", n1, dst1, n2, dst2);
-}
+	n1 = strlcat(dst1, src, 3);
+	n2 = ft_strlcat(dst2, src, 3);
+	printf("%zu: %s, %zu: %s\n", n1, dst1, n2, dst2);
+}*/
