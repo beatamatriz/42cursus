6d53ede899e2cc1d407918b7b9cdfa56e4377431
Beatriz Batista Ballesteros <bbatista@c1r6s6.42malaga.com>
2023-12-08 14:46:52 +0100

    oopsie arreglat

--- Libft/ft_tolower.c
+++ Libft/ft_tolower.c
@@ -6,15 +6,20 @@
 /*   By: bbatista <bbatista@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 17:03:02 by bbatista          #+#    #+#             */
-/*   Updated: 2023/11/27 17:03:30 by bbatista         ###   ########.fr       */
+/*   Updated: 2023/12/08 13:45:37 by bbatista         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
+
 int	ft_tolower(int c)
 {
-	if (isupper(c))
+	if (ft_isupper(c))
 		return (c + 32);
 	else
 		return (c);
--- Libft/ft_toupper.c
+++ Libft/ft_toupper.c
@@ -6,15 +6,20 @@
 /*   By: bbatista <bbatista@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 17:01:21 by bbatista          #+#    #+#             */
-/*   Updated: 2023/11/27 17:02:43 by bbatista         ###   ########.fr       */
+/*   Updated: 2023/12/08 13:45:48 by bbatista         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
+
 int	ft_toupper(int c)
 {
-	if (islower(c))
+	if (ft_islower(c))
 		return (c - 32);
 	else
 		return (c);
